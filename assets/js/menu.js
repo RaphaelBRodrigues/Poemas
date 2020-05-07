58988a7b9ae9c90419252d23ba03340555613194
Raphael4Vets <raphael@4vets.com.br>
2020-05-07 20:28:19 -0300

    FIX navbar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,10 @@ var click = 0;
 var menu;
 
 
+
+
     function menuClick() {
+        
     if((click%2) == 0){
         abrirMenu();
     }else{
@@ -11,18 +14,30 @@ var menu;
     click++;
 }
 function abrirMenu(){
-    menu.style.display = "inherit";
-    menu.style.opacity = "1";
-    menu.style.height = "16em";
-    menu.style.top = "4em";
+    if(screen.width<1000){
+        menu.style.display = "inherit";
+        menu.style.opacity = "1";
+        menu.style.height = "16em";
+        menu.style.top = "4em";
+    }
+ 
 }
 function fecharMenu(){
+    if(screen.width<1000){
+
     menu.style.opacity = "0";
     menu.style.height = "0";
     menu.style.top = "-16em";
+    }
 }
 window.onload = function(){
     menu  = document.getElementById("collapse");
     abrirMenu();
     fecharMenu();
+    if(screen.width>999){
+        menu.style.display = "inherit";
+        menu.style.opacity = "1";
+        menu.style.height = "4em";
+        menu.style.top = "4em";
+}
 }
